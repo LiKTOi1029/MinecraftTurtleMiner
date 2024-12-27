0274ef1cf20081d5c9f850a42f516fb536e93150
LiKTOi1029 <testingboy5689@gmail.com>
2024-12-27 12:54:41 +0330

    bugfixing line 112 and line 116 comparing function with number error

--- miner.lua
+++ miner.lua
@@ -109,11 +109,11 @@ function fBranchMining()
 		print("something is wrong with the value ("..tostring(branchNum)..") you inputted!")
 		fBranchMining()
 	end
-	if turtle.getFuelLevel < 15000 then
+	if turtle.getFuelLevel() < 15000 then
 		turtle.select(1)
 		repeat
 			turtle.refuel()
-		until turtle.getFuelLevel > 15000
+		until turtle.getFuelLevel() > 15000
 	end
 	turtle.up()
 	turtle.select(2)
