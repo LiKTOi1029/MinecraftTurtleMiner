f498ed4cacb08baabd31fb91351f674a3e4e83d4
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-12 21:10:09 +0330

    miner.lua finishing up __CHECK__() function

--- miner.lua
+++ miner.lua
@@ -8,29 +8,39 @@ function __CHECK__()
 	for num1 = 1, 5, 1 do
 		print(textutils.serialise(turtle.getItemDetail(num1,true)))
 		if turtle.getItemDetail(num1) == "minecraft:torch" then
-			print("success") 
+			print("Torches: success") 
 			cTorch["Details"], cTorch["Location"] = turtle.getItemDetail(num1), num1
+		else
+			error("No torches between slots 1 and 5")
 		end
 		if turtle.getItemDetail(num1) == "minecraft:coal_block" then
-			print("success")
+			print("CoalBlock: success")
 			cCoalBlock["Details"], cCoalBlock["Location"] = turtle.getItemDetail(num1), num1
+		else
+			error("No coal blocks between slots 1 and 5")
 		end
 		if turtle.getItemDetail(num1) == "minecraft:chest" then 
-			print("success")
+			print("Chest: success")
 			cChest["Details"], cChest["Location"] = turtle.getItemDetail(num1), num1
+		else
+			error("No chests between slots 1 and 5")
 		end
 		if turtle.getItemDetail(num1) == "mychunkloader:chunkloader" then
-			print("success")
+			print("Chunkloader: success")
 			cChunkloader["Details"], cChunkloader["Location"] = turtle.getItemDetail(num1), num1
+		else
+			error("No chunkloaders between slots 1 and 5")
 		end
 		if turtle.getItemDetail(num1) == "minecraft:cobblestone" then 
-			print("success")
+			print("Cobblestone: success")
 			cCobblestone["Details"], cCobblestone["Location"] = turtle.getItemDetail(num1), num1
+		else
+			error("No cobblestone between slots 1 and 5")
 		end
 	end
 	for num1 = 6, 16, 1 do
 		if turtle.getItemDetail(num1) then
-			error("Please empty all slots past slot 6. They are required for storage purposes.")
+			error("Please empty all slots past slot 5. They are required for random access storage.")
 		end
 	end
 	if turtle.getFuelLevel() < 20000 then
@@ -58,6 +68,7 @@ function __CHECK__()
 	end
 end
 function __MAIN__()
-	cImportantItems = 
+	local cImportantItems = __CHECK__()
+	
 end
 --https://pastebin.com/8BvSBn1K
