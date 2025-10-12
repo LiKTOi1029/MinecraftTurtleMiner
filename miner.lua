69b170582ad1988f6f6dc2e6083dbb5f2215de9d
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-12 21:36:55 +0330

    miner.lua added functions __FOUND_ORE__(), __MINING__(), and updated __CHECK__()

--- miner.lua
+++ miner.lua
@@ -1,46 +1,44 @@
-function __BEGIN__()
-	
-end
 function __CHECK__()
 	local cTorch, cCoalBlock, cChest, cCobblestone, cChunkloader = {}, {}, {}, {}, {}
 	term.write("Checking inventory, please wait\n")
 	os.sleep(1)
 	for num1 = 1, 5, 1 do
-		print(textutils.serialise(turtle.getItemDetail(num1,true)))
-		if turtle.getItemDetail(num1) == "minecraft:torch" then
+		local SelectedItem = textutils.serialise(turtle.getItemDetail(num1))["name"]
+		if SelectedItem == "minecraft:torch" then
 			print("Torches: success") 
-			cTorch["Details"], cTorch["Location"] = turtle.getItemDetail(num1), num1
+			cTorch["Details"], cTorch["Location"] = SelectedItem, num1
 		else
 			error("No torches between slots 1 and 5")
 		end
-		if turtle.getItemDetail(num1) == "minecraft:coal_block" then
+		if SelectedItem == "minecraft:coal_block" then
 			print("CoalBlock: success")
-			cCoalBlock["Details"], cCoalBlock["Location"] = turtle.getItemDetail(num1), num1
+			cCoalBlock["Details"], cCoalBlock["Location"] = SelectedItem, num1
 		else
 			error("No coal blocks between slots 1 and 5")
 		end
-		if turtle.getItemDetail(num1) == "minecraft:chest" then 
+		if SelectedItem == "minecraft:chest" then 
 			print("Chest: success")
-			cChest["Details"], cChest["Location"] = turtle.getItemDetail(num1), num1
+			cChest["Details"], cChest["Location"] = SelectedItem, num1
 		else
 			error("No chests between slots 1 and 5")
 		end
-		if turtle.getItemDetail(num1) == "mychunkloader:chunkloader" then
+		if SelectedItem == "mychunkloader:chunkloader" then
 			print("Chunkloader: success")
-			cChunkloader["Details"], cChunkloader["Location"] = turtle.getItemDetail(num1), num1
+			cChunkloader["Details"], cChunkloader["Location"] = SelectedItem, num1
 		else
 			error("No chunkloaders between slots 1 and 5")
 		end
-		if turtle.getItemDetail(num1) == "minecraft:cobblestone" then 
+		if SelectedItem == "minecraft:cobblestone" then 
 			print("Cobblestone: success")
-			cCobblestone["Details"], cCobblestone["Location"] = turtle.getItemDetail(num1), num1
+			cCobblestone["Details"], cCobblestone["Location"] = SelectedItem, num1
 		else
 			error("No cobblestone between slots 1 and 5")
 		end
 	end
 	for num1 = 6, 16, 1 do
-		if turtle.getItemDetail(num1) then
-			error("Please empty all slots past slot 5. They are required for random access storage.")
+		local SelectedItem = textutils.serialise(turtle.getItemDetail(num1))
+		if SelectedItem then
+			error("Please empty slot "..num1..". Slots past slot 5 are used for Random Access Storage.")
 		end
 	end
 	if turtle.getFuelLevel() < 20000 then
@@ -49,23 +47,18 @@ function __CHECK__()
 			turtle.refuel()
 		end
 	end
-	if not cTorch then
-		return nil
-	elseif not cCobblestone then
-		return nil
-	elseif not cCoalBlock then
-		return nil
-	elseif not cChunkloader then
-		return nil
-	elseif not cChest then
-		return nil
-	else
-		return {["Torches"] = cTorch,
-				["CoalBlock"] = cCoalBlock,
-				["Chest"] = cChest,
-				["Cobblestone"] = cCobblestone,
-				["Chunkloader"] = cChunkloader}
-	end
+	return {["Torches"] = cTorch,
+			["CoalBlock"] = cCoalBlock,
+			["Chest"] = cChest,
+			["Cobblestone"] = cCobblestone,
+			["Chunkloader"] = cChunkloader}
+end
+function __FOUND_ORE__()
+
+end
+function __MINING__()
+	local StartingBlock = "minecraft:torch"
+	
 end
 function __MAIN__()
 	local cImportantItems = __CHECK__()
