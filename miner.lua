65f3cf484ec16bb5f8b3a0b77bd5965ba896cd15
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-12 19:51:33 +0330

    miner.lua redoing main interface

--- miner.lua
+++ miner.lua
@@ -14,19 +14,5 @@ function __CHECK__()
 		if turtle.getItemDetail(num1) == "minecraft:torch" then print("success") end
 	end
 end
-function __SETTINGS__()
-	return nil
-end
-function __MAIN__()
-	term.write("What would you like to do?\n1)Setup/Check Inventory (recommended)\n2)Settings\n3)Begin\n4)Exit")
-	while true
-		local input = read("*l"):gsub("\n","")
-		if tonumber(input) == 1 then __CHECK__()
-		elseif tonumber(input) == 2 then __SETTINGS__()
-		elseif tonumber(input) == 3 then __BEGIN__() 
-		elseif tonumber(input) == 4 then break
-		else term.write("Please pick a valid option\n") end
-	end
-	--add some code to contact ender modem when that component is ready.
-end
+
 --https://pastebin.com/8BvSBn1K
