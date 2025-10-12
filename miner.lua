03a069487b4eff0c860d1b74cb5b760ae8a6cd95
LiKTOi1029 <testingboy5689@gmail.com>
2025-10-12 20:10:57 +0330

    miner.lua function __CHECK__() finally checks all of the required items

--- miner.lua
+++ miner.lua
@@ -1,28 +1,45 @@
-CHUNKLOADER = "mychunkloader:chunkloader"
 function __BEGIN__()
 	
 end
 function __CHECK__()
+	local cTorch, cCoalBlock, cChest, cCobblestone, cChunkloader = {}, {}, {}, {}, {}
 	term.write("Checking inventory, please wait\n")
 	os.sleep(1)
 	for num1 = 1, 5, 1 do
 		print(textutils.serialise(turtle.getItemDetail(num1,true)))
 		if turtle.getItemDetail(num1) == "minecraft:torch" then
 			print("success") 
-			
+			cTorch[1], cTorch[2] = turtle.getItemDetail(num1), num1
 		end
 		if turtle.getItemDetail(num1) == "minecraft:coal_block" then
 			print("success")
+			cCoalBlock[1], cCoalBlock[2] = turtle.getItemDetail(num1), num1
 		end
 		if turtle.getItemDetail(num1) == "minecraft:chest" then 
 			print("success")
+			cChest[1], cChest[2] = turtle.getItemDetail(num1), num1
 		end
-		if turtle.getItemDetail(num1) == CHUNKLOADER then
+		if turtle.getItemDetail(num1) == "mychunkloader:chunkloader" then
 			print("success")
+			cChunkloader[1], cChunkloader[2] = turtle.getItemDetail(num1), num1
 		end
 		if turtle.getItemDetail(num1) == "minecraft:cobblestone" then 
 			print("success")
+			cCobblestone[1], cCobblestone[2] = turtle.getItemDetail(num1), num1
 		end
+	end
+	if not cTorch then
+		return nil
+	elseif not cCobblestone then
+		return nil
+	elseif not cCoalBlock then
+		return nil
+	elseif not cChunkloader then
+		return nil
+	elseif not cChest then
+		return nil
+	else
+		return {cTorch, cCoalBlock, cChest, cCobblestone, cChunkloader}
 	end
 end
 function __MAIN__()
